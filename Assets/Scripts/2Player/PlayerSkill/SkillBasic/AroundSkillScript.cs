bea8b5afd877303e4d3d130ff0708a9613e03d1f
Honggoo Kang <hgk0501@naver.com>
2024-04-12 18:22:46 +0900

    카테고리별 skill script 추가

--- Assets/Scripts/2Player/PlayerSkill/SkillBasic/AroundSkillScript.cs
+++ Assets/Scripts/2Player/PlayerSkill/SkillBasic/AroundSkillScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RangeSkillScript : PlayerSkillScript
+public class AroundSkillScript:PlayerSkillScript
 {
 
 }
--- Assets/Scripts/2Player/PlayerSkill/SkillBasic/MeleeSkillScript.cs
+++ Assets/Scripts/2Player/PlayerSkill/SkillBasic/MeleeSkillScript.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class MeleeSkillScript : PlayerSkillScript
 {
-
+    // 추가적인 기능이 필요할 때 작성
 }
