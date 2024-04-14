32bb3371667649ef49e72eda37102beb3416b5b1
Honggoo Kang <hgk0501@naver.com>
2024-04-14 15:28:21 +0900

    Parabola_test prefab 구현(오류 있음)

--- Assets/Scripts/2Player/PlayerSkill/SkillBasic/ParabolaSkillScript.cs
+++ Assets/Scripts/2Player/PlayerSkill/SkillBasic/ParabolaSkillScript.cs
@@ -1,47 +1,20 @@
-//using System.Collections;
-//using UnityEngine;
+using System.Collections;
+using UnityEngine;
 
-//public class ParabolaSkillScript : ProjectileSkillScript
-//{
-//    protected Rigidbody m_rigid;
+public class ParabolaSkillScript : ProjectileSkillScript
+{
+    [SerializeField]
+    private float m_upperForce = 5;
 
-//    [SerializeField]
-//    private float m_moveSpeed = 6; // MoveSpeed SO 데려오기
-//    [SerializeField]
-//    private float m_upperForce = 5;
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        m_rigid.velocity = Vector3.up * m_upperForce;
+    }
 
-//    private Vector3 MoveDir { get; set; }
-//    public override void ReleaseTopool()
-//    {
-//        m_rigid.velocity = Vector3.zero;
-//        base.ReleaseTopool();
-//    }
-
-
-//    public void SetSkill(PlayerController _player, float _attack, float _magic, Vector3 _dir)
-//    {
-//        base.SetSkill(_player, _attack, _magic);
-//        MoveDir = new(_dir.x, 0, _dir.z);
-//    }
-
-//    public override void OnEnable()
-//    {
-//        base.OnEnable();
-//        m_rigid.velocity = Vector3.up * m_upperForce;
-//    }
-
-//    private void Awake()
-//    {
-//        m_rigid = GetComponent<Rigidbody>();
-//    }
-
-//    private override void FixedUpdate()
-//    {
-//        base.FixedUpdate();
-//        Vector3 vel = m_rigid.velocity;
-//        Vector3 dir = m_moveSpeed * MoveDir;
-//        vel.x = dir.x; vel.z = dir.z;
-//        m_rigid.velocity = vel;
-//        m_rigid.AddForce(Vector3.down * ValueDefine.PARABOLA_GRAVITY);
-//    }
-//}
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        m_rigid.AddForce(Vector3.down * ValueDefine.PARABOLA_GRAVITY);
+    }
+}
--- Assets/Scripts/2Player/PlayerSkill/SkillBasic/ProjectileSkillScript.cs
+++ Assets/Scripts/2Player/PlayerSkill/SkillBasic/ProjectileSkillScript.cs
@@ -2,28 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class ProjectileSkillScript : PlayerSkillScript
 {
+    protected Rigidbody m_rigid;
     private float MoveSpeed { get { return 5;/*m_scriptable.MoveSpeed;*/ } }
 
-    public Vector2 Direction { get; private set; }
-    public void SetSkill(PlayerController _player, float _attack, float _magic, Vector2 _dir)
+    private Vector3 MoveDir;
+
+    public override void ReleaseTopool()
     {
-        SetSkill(_player, _attack, _magic);
-        Direction = _dir;
+        m_rigid.velocity = Vector3.zero;
+        base.ReleaseTopool();
     }
 
+    public void SetSkill(PlayerController _player, float _attack, float _magic, Vector3 _dir)
+    {
+        SetSkill(_player, _attack, _magic);
+        MoveDir = new(_dir.x, 0, _dir.z);
+    }
 
+    private void Awake()
+    {
+        m_rigid = GetComponent<Rigidbody>();
+    }
 
-
-    /*
     public virtual void FixedUpdate()
     {
         Vector3 vel = m_rigid.velocity;
-        Vector3 dir = m_moveSpeed * MoveDir;
+        Vector3 dir = MoveSpeed * MoveDir;
         vel.x = dir.x; vel.z = dir.z;
         m_rigid.velocity = vel;
     }
-    */
 }
 
