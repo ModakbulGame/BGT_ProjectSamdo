d35797f854a99f41f46e6df582d312f3d3935c30
dlawotjr1999 <dlawotjr1999@gmail.com>
2024-05-09 00:32:39 +0900

    다음 대사 출력 난항

--- Assets/Scripts/3Object/32NPC/NPCDialogueScript.cs
+++ Assets/Scripts/3Object/32NPC/NPCDialogueScript.cs
@@ -12,17 +12,18 @@ public class NPCDialogueScript : MonoBehaviour      // 기존에 만들어져 있던 Oasi
     private TextMeshProUGUI m_TypingText;
     private string m_tmpText;
     [SerializeField]
-    private float m_Speed = 0.2f;
+    private float m_Speed = 0.1f;
 
     private Button[] m_btn;
     private EventTrigger m_trigger;
+    private IEnumerator m_coroutine;
 
     private bool Opened { get; set; }
     private bool ButtonClicked { get; set; }
 
     private NPCScript m_npc;
     private string[] m_dialogues;
-    private int m_dialogueCount;        
+    private int m_dialogueCount;
 
     public void SetNPC(NPCScript _npc)
     {
@@ -50,15 +51,14 @@ public class NPCDialogueScript : MonoBehaviour      // 기존에 만들어져 있던 Oasi
         gameObject.SetActive(false);
     }
 
-
     IEnumerator Typing(string _contents)
     {
-        m_TypingText.text = null;
+        m_TypingText.text = "";
 
         for (int i = 0; i < _contents.Length; i++)
         {
             m_TypingText.text += _contents[i];
-            if (ButtonClicked)
+            if (ButtonClicked)      // 좌클릭하면 전체 내용 한 번에 출력
             {
                 m_TypingText.text = _contents;
                 ButtonClicked = false;
@@ -68,30 +68,32 @@ public class NPCDialogueScript : MonoBehaviour      // 기존에 만들어져 있던 Oasi
         }
     }
 
+
     private void ShowAllDialogue(PointerEventData _data)
     {
         if (_data.button == PointerEventData.InputButton.Left)
         {
             ButtonClicked = true;
-            StopCoroutine(Typing(m_dialogues[m_dialogueCount]));
+            StopCoroutine(m_coroutine);
         }
     }
 
     private void NextDialogue()
     {
-        StopCoroutine(m_dialogues[m_dialogueCount]);
+        StopCoroutine(m_coroutine);
         m_TypingText.text = "";
         if (m_dialogueCount < m_npc.m_NPCDialogue.Length)
         {
-            m_dialogueCount++;
-            StartCoroutine(Typing(m_dialogues[m_dialogueCount]));
+            m_coroutine = Typing(m_dialogues[m_dialogueCount++]);
+            // Debug.Log(m_dialogueCount);
+            StartCoroutine(m_coroutine);
         }
-
     }
 
     private void SetComps()
     {
         m_dialogueCount = 0;
+        m_coroutine = Typing(m_dialogues[0]);
         m_trigger = GetComponent<EventTrigger>();
         FunctionDefine.AddEvent(m_trigger, EventTriggerType.PointerClick, ShowAllDialogue);
         m_btn = GetComponentsInChildren<Button>();
