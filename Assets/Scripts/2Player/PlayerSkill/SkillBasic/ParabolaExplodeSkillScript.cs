efb4c249715b031c57e805c8cb19a6acea408166
Honggoo Kang <hgk0501@naver.com>
2024-05-18 21:52:26 +0900

    폭발 스킬에 관한 고찰

--- Assets/Scripts/2Player/PlayerSkill/SkillBasic/ParabolaExplodeSkillScript.cs
+++ Assets/Scripts/2Player/PlayerSkill/SkillBasic/ParabolaExplodeSkillScript.cs
@@ -6,12 +6,26 @@ public class ParabolaExplodeSkillScript : ParabolaSkillScript
 {
     [SerializeField]
     private GameObject m_skillExplode;
+    [SerializeField]
+    private Vector2 m_spawnRangeMax;
+    [SerializeField]
+    private Vector2 m_spawnRangeMin;
+    private readonly float NumberOfPrefabsSpawn = 3;
 
     private void ExplodeSkill()
     {
+        float randomX = Random.Range(m_spawnRangeMin.x, m_spawnRangeMax.x);
+        float randomZ = Random.Range(m_spawnRangeMin.y, m_spawnRangeMax.y);
+        Vector3 spawnPosition = new Vector3(randomX, 0, randomZ);
         m_skillExplode.SetActive(true);
         m_skillExplode.transform.SetParent(null);
         PlayerSkillScript explosion = m_skillExplode.GetComponent<PlayerSkillScript>();
         explosion.SetDamage(5);
     }
+
+    public override void ReleaseToPool()
+    {
+        base.ReleaseToPool();
+    }
+
 }
