ed1ce1e451a7779cd90de89da0df9f94ee667919
Honggoo Kang <hgk0501@naver.com>
2024-05-23 01:38:29 +0900

    object와 지형 상호작용중

--- Assets/Scripts/2Player/PlayerSkill/SkillBasic/ExplodeScript.cs
+++ Assets/Scripts/2Player/PlayerSkill/SkillBasic/ExplodeScript.cs
@@ -7,18 +7,29 @@ public class ExplodeScript : ObjectAttackScript
 {
     [SerializeField]
     private VisualEffect m_explosionEffect;
-
-    private Transform ReturnTransform;
+    [SerializeField]
+    public override float Damage => base.Damage;
 
     public void SetDamage(ObjectScript _attacker, float _damage, float _time)
     {
         SetAttack(_attacker, _damage);
-        StartCoroutine(LoseDamage(_time));
+        StartCoroutine(Explosion());
     }
 
-    private IEnumerator LoseDamage(float _time)
+    private IEnumerator Explosion()
     {
-        yield return new WaitForSeconds(_time);
-        AttackOff();
+        m_explosionEffect.Play();
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+
+    private void CheckExplosionTrigger(Collider _other)
+    {
+        IHittable hittable = _other.GetComponentInParent<IHittable>();
+        hittable ??= _other.GetComponentInChildren<IHittable>();
+        if (hittable == null) { return; }
+        if (hittable.IsPlayer) { return; }
+        Vector3 point = _other.ClosestPoint(transform.position);
+        GiveDamage(hittable, point);
     }
 }
--- Assets/Scripts/2Player/PlayerSkill/SkillBasic/ParabolaExplodeScript.cs
+++ Assets/Scripts/2Player/PlayerSkill/SkillBasic/ParabolaExplodeScript.cs
@@ -5,8 +5,6 @@ public class ParabolaExplodeScript : ParabolaSkillScript
 {
     [SerializeField]
     private GameObject[] m_skillExplosion;
-    [SerializeField]
-    private float m_explodeRange = 5f;
 
     private void ExplodeSkill()
     {
@@ -14,26 +12,19 @@ public class ParabolaExplodeScript : ParabolaSkillScript
         {
             m_skillExplosion[i].SetActive(true);
             m_skillExplosion[i].transform.SetParent(null);
-            PlayerSkillScript explode = m_skillExplosion[i].GetComponent<PlayerSkillScript>();
-            explode.SetDamage(5);
         }
     }
-    public override void CheckSkillTrigger(Collider _other)
+    public override void CollideTarget()
     {
-        IHittable hittable = _other.GetComponentInParent<IHittable>();
-        hittable ??= _other.GetComponentInChildren<IHittable>();
-        if (hittable == null) { return; }
-        if (hittable.IsPlayer) { return; }
-        Vector3 point = _other.ClosestPoint(transform.position);
-        GiveDamage(hittable, point);
         ExplodeSkill();
-        CollideTarget();
     }
 
     private void OnTriggerEnter(Collider _other)
     {
-        GameObject gameObject = _other.gameObject;
         int groundLayer = LayerMask.NameToLayer("Ground");
-        if (gameObject.layer == groundLayer) { ExplodeSkill(); }
+        if(_other is MeshCollider &&_other.gameObject.layer == groundLayer)
+        {
+            ExplodeSkill();
+        }
     }
 }
