4680672a1554685f18dbc69c50a422c9b3e0fdd1
Hwagwana <rivsh030@gmail.com>
2024-05-22 18:10:49 +0900

    무기 오류 수정
    사소한 오류

--- Assets/Scripts/3Object/31Monster/MonsterList/Uum/UumScript.cs
+++ Assets/Scripts/3Object/31Monster/MonsterList/Uum/UumScript.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class UumScript : MonsterScript
 {
+    [SerializeField]
+    private VisualEffect m_headFire;
 
 
-
+    public override void StartDissolve()
+    {
+        base.StartDissolve();
+        m_headFire.Stop();
+    }
 }
