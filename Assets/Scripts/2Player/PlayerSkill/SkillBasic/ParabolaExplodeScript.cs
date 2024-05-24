96ef5c1ee53a20b8adeec9b96579f1ec52353f4f
Hwagwana <rivsh030@gmail.com>
2024-05-24 17:36:18 +0900

    Update ParabolaExplodeScript.cs

--- Assets/Scripts/2Player/PlayerSkill/SkillBasic/ParabolaExplodeScript.cs
+++ Assets/Scripts/2Player/PlayerSkill/SkillBasic/ParabolaExplodeScript.cs
@@ -5,12 +5,14 @@ public class ParabolaExplodeScript : ParabolaSkillScript
 {
     [SerializeField]
     private GameObject[] m_skillExplosion;
+    private Vector3[] SkillOffset;
 
     private void ExplodeSkill()
     {
         for (int i = 0; i < m_skillExplosion.Length; i++)   
         {
             m_skillExplosion[i].SetActive(true);
+            m_skillExplosion[i].transform.localPosition = SkillOffset[i];
             m_skillExplosion[i].transform.SetParent(null);
             ExplodeScript explode = m_skillExplosion[i].GetComponent<ExplodeScript>();
             explode.SetDamage(m_attacker, 5, 1);
@@ -30,5 +32,12 @@ public class ParabolaExplodeScript : ParabolaSkillScript
         int groundLayer = LayerMask.NameToLayer("Ground");
         if (gameObject.layer == groundLayer) { ExplodeSkill(); }
         CheckSkillTrigger(_other);
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        SkillOffset = new Vector3[m_skillExplosion.Length];
+        for(int i = 0; i<3; i++) { SkillOffset[i] = m_skillExplosion[i].transform.localPosition; }
     }
 }
