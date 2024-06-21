7e582dd249aac4c51be0ddf5b60ec7232626c2bd
dlawotjr1999 <dlawotjr1999@gmail.com>
2024-06-22 02:00:16 +0900

    퀘스트 UI 만드는 중

--- Assets/Scripts/7UI/StoryUI/QuestUIScript.cs
+++ Assets/Scripts/7UI/StoryUI/QuestUIScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class QuestUIScript : BaseUI
 {
+    private QuestUIElmScript[] m_elms;
+    private QuestUIDescScript m_desc;
 
     public override void OpenUI()
     {
@@ -25,5 +27,9 @@ public class QuestUIScript : BaseUI
     public override void SetComps()
     {
         base.SetComps();
+        m_elms = GetComponentsInChildren<QuestUIElmScript>();
+        foreach(QuestUIElmScript elm in m_elms) elm.SetComps();
+        m_desc = GetComponentInChildren<QuestUIDescScript>();
+        m_desc.SetComps();
     }
 }
