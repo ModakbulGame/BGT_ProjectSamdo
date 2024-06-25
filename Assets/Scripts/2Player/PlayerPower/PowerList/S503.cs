a9f1b5cea8cdcf75f4a5856c687ab1660ed2872a
Honggoo Kang <hgk0501@naver.com>
2024-06-25 12:31:31 +0900

    power prefab 생성 및 추가적인 power trail 추가

--- Assets/Scripts/2Player/PlayerPower/PowerList/S503.cs
+++ Assets/Scripts/2Player/PlayerPower/PowerList/S503.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class S503 : PlayerPowerScript
 {
-    
+    [SerializeField]
+    private float m_buffTimeGet;
+    private Dictionary<IHittable, float> m_buffTimeCount = new();
+
+    private void GiveBuff(PlayerController _player)
+    {
+        if (m_buffTimeCount.ContainsKey(_player))
+            return;
+        m_buffTimeCount[_player] = m_buffTimeGet;
+    }
 }
